698abd8d7d8eed6922fe6868b1fe7bf2a07a49a3
hiagobalbino <hiagobalbinoo@gmail.com>
2023-11-11 20:44:41 -0300

    improve initial page message

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>I'm learning to improve my programming skills and be a better developer!</h1>
+    return <h1>I'm learning to improve my programming skills and be a better developer! 🚀</h1>
 }
 
 export default Home;
