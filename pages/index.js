f2232837ce8f72e50fbfafe6c9da257dd0631f8e
hiagobalbino <hiagobalbinoo@gmail.com>
2023-11-15 19:12:14 -0300

    add scripts `lint:check` and `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>I'm learning to improve my programming skills and be a better developer! 🚀</h1>
+  return (
+    <h1>
+      I'm learning to improve my programming skills and be a better developer!
+      🚀
+    </h1>
+  );
 }
 
 export default Home;
