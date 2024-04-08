4701e557fa15b1a07468c2c6570a34e9db9202f2
hiagobalbino <hiagobalbinoo@gmail.com>
2024-04-07 22:14:20 -0300

    feat: add database.js and use of env var

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  await database.query("SELECT 1 + 1 AS sum;");
+
   response.status(200).json({
     status: "OK",
   });
