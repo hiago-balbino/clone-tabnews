010ddcaf58cc74aff5166635f12129460f2bb7b4
hiagobalbino <hiagobalbinoo@gmail.com>
2024-04-13 19:56:03 -0300

    feat: finished status endpoint for database dependency

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,30 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  await database.query("SELECT 1 + 1 AS sum;");
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseRowValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query("SHOW max_connections;");
+  const databaseMaxConnectionsValue = databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT COUNT(*)::INT FROM pg_stat_activity WHERE datname = $1;", 
+    values: [databaseName]
+  });
+  const databseOpenedConnectionsValue = databaseOpenedConnectionsResult.rows[0].count;
 
   response.status(200).json({
-    status: "OK",
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseRowValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databseOpenedConnectionsValue
+      }
+    }
   });
 };
 
