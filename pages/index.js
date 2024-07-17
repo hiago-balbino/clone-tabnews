d0c3907a15e353009709c7baf070450d77708656
Hiago Balbino <hiagobalbinoo@gmail.com>
2024-07-16 23:37:24 -0300

    script: add eslint checks for linting (#17)
    * script: add lint:eslint:check script with ESLint
    
    * script: add ESLint script on linting CI
    
    * fix: ESLint warnings

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home() {
   return (
     <h1>
-      I'm learning to improve my programming skills and be a better developer!
+      I am learning to improve my programming skills and be a better developer!
       🚀
     </h1>
   );
